182df96c97df1d7257fcb257b620a739ac5da24c
hebertcisco <hebertcisco@outlook.com>
2022-02-16 17:13:39 +0000

    refactor:

--- ps1/import.ps1
+++ ps1/import.ps1
@@ -1,3 +1,3 @@
 #!/usr/bin/env pwsh
 # import docker-build.ps1
-Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://docker-powershell.vercel.app/ps1/docker-build.ps1 -UseBasicParsing | iex
+New-Module -Name ImportFromURL -ScriptBlock ([Scriptblock]::Create((New-Object System.Net.WebClient).DownloadString("https://docker-powershell.vercel.app/psm1/docker-build.psm1")))
