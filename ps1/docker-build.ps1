7c79d4e3343ac2bbb6d8bb8b26b5be0a758e6415
hebertcisco <hebertcisco@outlook.com>
2022-02-16 17:04:39 +0000

    refactor: :technologist: add shibang flags

--- ps1/docker-build.ps1
+++ ps1/docker-build.ps1
@@ -1,1 +1,2 @@
+#!/usr/bin/env pwsh
 New-Module -Name ImportFromURL -ScriptBlock ([Scriptblock]::Create((New-Object System.Net.WebClient).DownloadString("https://docker-powershell.vercel.app/psm1/docker-build.psm1")))
--- ps1/example.ps1
+++ ps1/example.ps1
@@ -1,1 +1,2 @@
+#!/usr/bin/env pwsh
 Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://docker-powershell.vercel.app/ps1/example.ps1 -UseBasicParsing | iex
