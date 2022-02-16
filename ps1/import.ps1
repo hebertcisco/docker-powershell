55a880146eea2283298f39b47786e89f527d0d6a
hebertcisco <hebertcisco@outlook.com>
2022-02-16 17:05:19 +0000

    feat: :hammer: add import script

---
+++ ps1/import.ps1
@@ -0,0 +1,3 @@
+#!/usr/bin/env pwsh
+# import docker-build.ps1
+Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://docker-powershell.vercel.app/ps1/docker-build.ps1 -UseBasicParsing | iex
